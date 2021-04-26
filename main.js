64dc9ad16e75066050f40745397bbbac83122af8
Parv17k <khatriparv@gmail.com>
2021-04-26 18:28:14 -0400

    solution file main.js code implementation done

--- main.js
+++ main.js
@@ -14,3 +14,41 @@ let map = {
     9: "Nine",
     0: "Zero"
 };
+
+//helper Functions
+function getResultString(input) {
+    let results = [];
+
+    for (let i = 2; i < input.length; i++)
+        results.push(generateStringFromInteger(input[i]));
+
+
+    return results;
+}
+
+function generateStringFromInteger(num) {
+    let output="";
+    
+    if (num == 0) return "Zero";
+    
+    while (num != 0) {
+        let temp = num % 10;
+        num = parseInt(num / 10);
+        output = map[temp] + output;
+    }
+    
+    return output;
+
+}
+
+// Executing the function to get the result array;
+let result = getResultString(args);
+
+
+// printing std output of result Array. 
+for (let i = 0; i < result.length; i++)
+    if (i != result.length - 1)
+        process.stdout.write(result[i] + ",");
+    else
+        process.stdout.write(result[i]);
+
